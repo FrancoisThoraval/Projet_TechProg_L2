5ee67b8e68c4cd98508c5292cc9b17bf2005a871
ThePanda33 <francoisthoraval@hotmail.fr>
2016-10-26 19:59:13 +0200

    creation installateur

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+echo "Installation de la bataille navale ..."
+echo "Veuillez patienter"
+# make ./Bataille_Navale/
+# Si retour reussi
+retour=$?
+if [[ retour == 0 ]]; then
+  echo "Installation réussie !"
+fi
+
+echo "Installation du tetris ..."
+echo "Veuillez patienter"
+
+make ./Tetris/main.c
+retour=$?
+if [[ retour == 0 ]]; then
+  echo "Installation réussie !"
+fi
+echo "Success !!"
